6e85c2ad9b58243f33e1146bff3307bbb1a526f6
Mario Gabriell <mario_gk@hotmail.com>
2015-11-12 16:03:33 -0200

    Initial Release Galio+Thresh

---
+++ ThreshHu3 Reborn/AddonTemplate/Config.cs
@@ -0,0 +1,344 @@
+﻿using EloBuddy.SDK.Menu;
+using EloBuddy.SDK.Menu.Values;
+using Color = System.Drawing.Color;
+
+// ReSharper disable ConvertPropertyToExpressionBody
+// ReSharper disable MemberCanBePrivate.Global
+// ReSharper disable InconsistentNaming
+// ReSharper disable MemberHidesStaticFromOuterClass
+
+namespace AddonTemplate
+{
+    public static class Config
+    {
+        private const string MenuName = "AddonTemplate";
+
+        private static readonly Menu Menu;
+
+        static Config()
+        {
+            // Initialize the menu
+            Menu = MainMenu.AddMenu(MenuName, MenuName.ToLower());
+            Menu.AddGroupLabel("AddonTemplate");
+            Menu.AddLabel("Made By: MarioGK", 50);
+
+            // Initialize the modes
+            Modes.Initialize();
+        }
+
+        public static void Initialize()
+        {
+        }
+
+        public static class Modes
+        {
+            private static readonly Menu ModesMenu, DrawMenu;
+
+            static Modes()
+            {
+                ModesMenu = Menu.AddSubMenu("Modes");
+
+                Combo.Initialize();
+                Menu.AddSeparator();
+                Harass.Initialize();
+                Menu.AddSeparator();
+                LaneClear.Initialize();
+                Menu.AddSeparator();
+                LastHit.Initialize();
+
+                DrawMenu = Menu.AddSubMenu("Draw");
+                Draw.Initialize();
+            }
+
+            public static void Initialize()
+            {
+            }
+
+            public static class Combo
+            {
+                private static readonly CheckBox _useQ;
+                private static readonly CheckBox _useW;
+                private static readonly CheckBox _useE;
+                private static readonly CheckBox _useR;
+
+                public static bool UseQ
+                {
+                    get { return _useQ.CurrentValue; }
+                }
+
+                public static bool UseW
+                {
+                    get { return _useW.CurrentValue; }
+                }
+
+                public static bool UseE
+                {
+                    get { return _useE.CurrentValue; }
+                }
+
+                public static bool UseR
+                {
+                    get { return _useR.CurrentValue; }
+                }
+
+                static Combo()
+                {
+                    // Initialize the menu values
+                    ModesMenu.AddGroupLabel("Combo");
+                    _useQ = ModesMenu.Add("comboQ", new CheckBox("Use Q"));
+                    _useW = ModesMenu.Add("comboW", new CheckBox("Use W"));
+                    _useE = ModesMenu.Add("comboE", new CheckBox("Use E"));
+                    _useR = ModesMenu.Add("comboR", new CheckBox("Use R", false)); // Default false
+                }
+
+                public static void Initialize()
+                {
+                }
+            }
+
+            public static class Harass
+            {
+                private static readonly CheckBox _useQ;
+                private static readonly CheckBox _useW;
+                private static readonly CheckBox _useE;
+                private static readonly CheckBox _useR;
+
+                public static bool UseQ
+                {
+                    get { return _useQ.CurrentValue; }
+                }
+
+                public static bool UseW
+                {
+                    get { return _useW.CurrentValue; }
+                }
+
+                public static bool UseE
+                {
+                    get { return _useE.CurrentValue; }
+                }
+
+                public static bool UseR
+                {
+                    get { return _useR.CurrentValue; }
+                }
+
+                static Harass()
+                {
+                    // Initialize the menu values
+                    ModesMenu.AddGroupLabel("Harass");
+                    _useQ = ModesMenu.Add("harassQ", new CheckBox("Use Q"));
+                    _useW = ModesMenu.Add("harassW", new CheckBox("Use W"));
+                    _useE = ModesMenu.Add("harassE", new CheckBox("Use E"));
+                    _useR = ModesMenu.Add("harassR", new CheckBox("Use R", false)); // Default false
+                }
+
+                public static void Initialize()
+                {
+                }
+            }
+
+            public static class LaneClear
+            {
+                private static readonly CheckBox _useQ;
+                private static readonly CheckBox _useW;
+                private static readonly CheckBox _useE;
+                private static readonly CheckBox _useR;
+
+                public static bool UseQ
+                {
+                    get { return _useQ.CurrentValue; }
+                }
+
+                public static bool UseW
+                {
+                    get { return _useW.CurrentValue; }
+                }
+
+                public static bool UseE
+                {
+                    get { return _useE.CurrentValue; }
+                }
+
+                public static bool UseR
+                {
+                    get { return _useR.CurrentValue; }
+                }
+
+                static LaneClear()
+                {
+                    // Initialize the menu values
+                    ModesMenu.AddGroupLabel("LaneClear");
+                    _useQ = ModesMenu.Add("laneQ", new CheckBox("Use Q"));
+                    _useW = ModesMenu.Add("laneW", new CheckBox("Use W"));
+                    _useE = ModesMenu.Add("laneE", new CheckBox("Use E"));
+                    _useR = ModesMenu.Add("laneR", new CheckBox("Use R", false)); // Default false
+                }
+
+                public static void Initialize()
+                {
+                }
+            }
+
+            public static class LastHit
+            {
+                private static readonly CheckBox _useQ;
+                private static readonly CheckBox _useW;
+                private static readonly CheckBox _useE;
+                private static readonly CheckBox _useR;
+
+                public static bool UseQ
+                {
+                    get { return _useQ.CurrentValue; }
+                }
+
+                public static bool UseW
+                {
+                    get { return _useW.CurrentValue; }
+                }
+
+                public static bool UseE
+                {
+                    get { return _useE.CurrentValue; }
+                }
+
+                public static bool UseR
+                {
+                    get { return _useR.CurrentValue; }
+                }
+
+                static LastHit()
+                {
+                    // Initialize the menu values
+                    ModesMenu.AddGroupLabel("LastHit");
+                    _useQ = ModesMenu.Add("lastQ", new CheckBox("Use Q"));
+                    _useW = ModesMenu.Add("lastW", new CheckBox("Use W"));
+                    _useE = ModesMenu.Add("lastE", new CheckBox("Use E"));
+                    _useR = ModesMenu.Add("lastR", new CheckBox("Use R", false)); // Default false
+                }
+
+                public static void Initialize()
+                {
+                }
+            }
+
+            public static class Draw
+            {
+                private static readonly CheckBox _drawHealth;
+                private static readonly CheckBox _drawQ;
+                private static readonly CheckBox _drawW;
+                private static readonly CheckBox _drawE;
+                private static readonly CheckBox _drawR;
+
+                private static readonly CheckBox _drawReady;
+
+                public static bool DrawHealth
+                {
+                    get { return _drawHealth.CurrentValue; }
+                }
+
+                public static bool DrawQ
+                {
+                    get { return _drawQ.CurrentValue; }
+                }
+
+                public static bool DrawW
+                {
+                    get { return _drawW.CurrentValue; }
+                }
+
+                public static bool DrawE
+                {
+                    get { return _drawE.CurrentValue; }
+                }
+
+                public static bool DrawR
+                {
+                    get { return _drawR.CurrentValue; }
+                }
+
+                public static bool DrawReady
+                {
+                    get { return _drawReady.CurrentValue; }
+                }
+
+
+                public static Color colorHealth
+                {
+                    get { return DrawMenu.GetColor("colorHealth"); }
+                }
+
+                public static Color colorQ
+                {
+                    get { return DrawMenu.GetColor("colorQ"); }
+                }
+
+                public static Color colorW
+                {
+                    get { return DrawMenu.GetColor("colorW"); }
+                }
+
+                public static Color colorE
+                {
+                    get { return DrawMenu.GetColor("colorE"); }
+                }
+
+                public static Color colorR
+                {
+                    get { return DrawMenu.GetColor("colorR"); }
+                }
+
+                public static float _widthQ
+                {
+                    get { return DrawMenu.GetWidth("widthQ"); }
+                }
+
+                public static float _widthW
+                {
+                    get { return DrawMenu.GetWidth("widthW"); }
+                }
+
+                public static float _widthE
+                {
+                    get { return DrawMenu.GetWidth("widthE"); }
+                }
+
+                public static float _widthR
+                {
+                    get { return DrawMenu.GetWidth("widthR"); }
+                }
+
+                static Draw()
+                {
+                    DrawMenu.AddGroupLabel("Draw");
+                    _drawReady = DrawMenu.Add("drawReady", new CheckBox("Draw Only If The Spells Are Ready.", false));
+                    DrawMenu.AddSeparator();
+                    DrawMenu.AddLabel("Reload is required to aply the changes made in the damage indicator");
+                    _drawHealth = DrawMenu.Add("drawHealth", new CheckBox("Draw Damage in HealthBar"));
+                    DrawMenu.AddColorItem("colorHealth");
+                    DrawMenu.AddSeparator();
+                    //Q
+                    _drawQ = DrawMenu.Add("drawQ", new CheckBox("Draw Q"));
+                    DrawMenu.AddColorItem("colorQ");
+                    DrawMenu.AddWidthItem("widthQ");
+                    //W
+                    _drawW = DrawMenu.Add("drawW", new CheckBox("Draw W"));
+                    DrawMenu.AddColorItem("colorW");
+                    DrawMenu.AddWidthItem("widthW");
+                    //E
+                    _drawE = DrawMenu.Add("drawE", new CheckBox("Draw E"));
+                    DrawMenu.AddColorItem("colorE");
+                    DrawMenu.AddWidthItem("widthE");
+                    //R
+                    _drawR = DrawMenu.Add("drawR", new CheckBox("Draw R"));
+                    DrawMenu.AddColorItem("colorR");
+                    DrawMenu.AddWidthItem("widthR");
+                }
+
+                public static void Initialize()
+                {
+                }
+            }
+        }
+    }
+}
